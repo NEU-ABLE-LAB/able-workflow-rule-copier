16fb6cfec538ba6b67f9687be30bd706894d6078
thisismikekane <mi.kane@northeastern.edu>
2025-06-10 14:52:35 -0400

    Initial commit

---
+++ scripts/conda_update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Update the conda environment file `environment-py312-dev.yaml`
+# to reflect any changes to pyproject.toml
+./hooks/pyproject2conda.py pyproject.toml
+
+# Update the conda environment
+conda env update --name able-workflow-rule-copier-dev --file environment-py312-dev.yaml --prune
